697eff5796631f6c3a79699ce466ce13bc29b824
Michal Gruszka <m.gruszka215@gmail.com>
2019-08-21 17:31:01 +0200

    Create model for shortened url

---
+++ src/main/java/dev/michalgruszka/model/ShortUrl.java
@@ -0,0 +1,58 @@
+package dev.michalgruszka.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "short_url")
+public class ShortUrl {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "original_url", unique = true)
+	private String originalUrl;
+	
+	@Column(name = "short_uri", unique = true)
+	private String shortUrl;
+	
+	
+	protected ShortUrl() {}
+	
+	public ShortUrl(String originalUrl, String shortUrl) {
+		this.originalUrl = originalUrl;
+		this.shortUrl = shortUrl;
+	}
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getOriginalUrl() {
+		return originalUrl;
+	}
+
+	public void setOriginalUrl(String originalUrl) {
+		this.originalUrl = originalUrl;
+	}
+
+	public String getShortUrl() {
+		return shortUrl;
+	}
+
+	public void setShortUrl(String shortUrl) {
+		this.shortUrl = shortUrl;
+	}
+	
+	
+}
