07176b2b6bb02980db36052aa28e25ed93b123e3
Michal Gruszka <m.gruszka215@gmail.com>
2019-08-21 17:34:53 +0200

    Add persistence related setup

--- src/main/java/dev/michalgruszka/config/AppConfig.java
+++ src/main/java/dev/michalgruszka/config/AppConfig.java
@@ -1,8 +1,21 @@
 package dev.michalgruszka.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -10,8 +23,21 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
+@EnableTransactionManagement
+@EnableJpaRepositories("dev.michalgruszka.repository")
 @ComponentScan("dev.michalgruszka")
+@PropertySource("classpath:database.properties")
 public class AppConfig {
+	
+	private final String PROPERTY_DRIVER = "driver";
+	private final String PROPERTY_URL = "url";
+	private final String PROPERTY_USERNAME = "user";
+	private final String PROPERTY_PASSWORD = "password";
+	private final String PROPERTY_SHOW_SQL = "hibernate.show_sql";
+	private final String PROPERTY_DIALECT = "hibernate.dialect";
+
+	@Autowired
+	Environment environment;
 	
 	@Bean
 	public ViewResolver viewResolver() {
@@ -20,5 +46,39 @@ public class AppConfig {
 		viewResolver.setPrefix("WEB-INF/jsp/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
+	}
+	
+	@Bean
+	LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean lfb = new LocalContainerEntityManagerFactoryBean();
+		lfb.setDataSource(dataSource());
+		lfb.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+		lfb.setPackagesToScan("dev.michalgruszka.model");
+		lfb.setJpaProperties(hibernateProps());
+		return lfb;
+	}
+
+	@Bean
+	DataSource dataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setUrl(environment.getProperty(PROPERTY_URL));
+		ds.setUsername(environment.getProperty(PROPERTY_USERNAME));
+		ds.setPassword(environment.getProperty(PROPERTY_PASSWORD));
+		ds.setDriverClassName(environment.getProperty(PROPERTY_DRIVER));
+		return ds;
+	}
+
+	Properties hibernateProps() {
+		Properties properties = new Properties();
+		properties.setProperty(PROPERTY_DIALECT, environment.getProperty(PROPERTY_DIALECT));
+		properties.setProperty(PROPERTY_SHOW_SQL, environment.getProperty(PROPERTY_SHOW_SQL));
+		return properties;
+	}
+
+	@Bean
+	JpaTransactionManager transactionManager() {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+		return transactionManager;
 	}
 }
