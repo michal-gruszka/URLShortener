53302bdad0ef2435d1f75bfac0ef92a1c85b2ed0
Michal Gruszka <m.gruszka215@gmail.com>
2019-08-29 14:19:49 +0200

    Create SQL function to manage url-code assignment

---
+++ src/main/sql/get_code_function.sql
@@ -0,0 +1,15 @@
+﻿CREATE OR REPLACE FUNCTION get_code_for(originalUrl text)
+RETURNS text AS
+$func$
+DECLARE
+  shortUrl varchar;
+BEGIN
+SELECT short_url FROM url WHERE original_url = originalUrl INTO shortUrl;
+IF shortUrl IS NULL THEN
+	SELECT code FROM code_pool INTO shortUrl;
+	INSERT INTO url(original_url, short_url) VALUES (originalUrl, shortUrl);
+	DELETE FROM code_pool WHERE code = shortUrl;
+END IF;
+RETURN shortUrl;
+END
+$func$ LANGUAGE plpgsql;
